326c0324d9ed88e288f3e9c2ab6c2a841887d7e2
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-05-27 00:12:27 +0200

    Added base service interface for managing books with default implementation

---
+++ src/main/java/com/piotrgrochowiecki/project_template/service/BookService.java
@@ -0,0 +1,4 @@
+package com.piotrgrochowiecki.project_template.service;
+
+public interface BookService {
+}
---
+++ src/main/java/com/piotrgrochowiecki/project_template/service/DefaultBookService.java
@@ -0,0 +1,7 @@
+package com.piotrgrochowiecki.project_template.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultBookService implements BookService {
+}
